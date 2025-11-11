198d392eca4b327cbd543c4bac2e3e706002c4f4
Nafisa <66S2026@utas.edu.om>
2025-11-11 09:43:04 +0400

    first upload

---
+++ client/src/Store/store.js
@@ -0,0 +1,30 @@
+import { configureStore } from "@reduxjs/toolkit";
+import usersReducer from "../Features/UserSlice.js";
+import postReducer from "../Features/PostSlice";
+
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; 
+import { combineReducers } from "redux";
+
+
+const persistConfig = {
+  key: "reduxstore",
+  storage,
+};
+
+
+const rootReducer = combineReducers({
+  users: usersReducer,
+  posts: postReducer,
+});
+
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistore = persistStore(store);
+
+export { store, persistore };
